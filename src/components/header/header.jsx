cc16d73adb2e0f7bbf25f072ed8fd0fe3751d6be
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-20 14:52:58 +0700

    Integrate NavMenu; add animations.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 import './header.scss'
 
-export const Header = () => {
+export const Header = ({ toggleMenu }) => {
   return (
     <nav className="header-nav">
       <div className="logo">LOGO</div>
-      <div className="menu-button">MENU</div>
+      <div className="menu-button" onClick={toggleMenu}>MENU</div>
     </nav>
   )
 }
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import { Header } from '../header/header'
 import { Footer } from '../footer/footer'
 
 import './layout.scss'
+import { NavMenu } from '../nav/nav-menu'
 
 export const Layout = ({ children }) => {
   const layoutRef = useRef(null)
   const [scrollProgress, setScrollProgress] = useState(0)
+  const [menuOpen, setMenuOpen] = useState(false)
+  console.log('menuOpen: ', menuOpen)
 
   useEffect(() => {
     const totalHeight = layoutRef.current.clientHeight
@@ -15,7 +19,7 @@ export const Layout = ({ children }) => {
 
     window.addEventListener('scroll', () => {
       const currentScroll = window.scrollY
-      
+
       setScrollProgress((currentScroll / maxScroll) * 100.1)
     })
     return () => {
@@ -25,9 +29,36 @@ export const Layout = ({ children }) => {
 
   return (
     <main className='layout' ref={layoutRef}>
-      <Header />
+      <AnimatePresence>
+        {menuOpen && (
+          <motion.div
+            initial={{ x: '-100vw' }}
+            animate={{
+              x: 0,
+              transition: {
+                duration: 0.4,
+                ease: [0.6, 0.05, -0.01, 0.9],
+              },
+            }}
+            exit={{
+              x: '-100vw',
+              transition: {
+                duration: 0.4,
+                ease: [0.6, 0.05, -0.01, 0.9],
+              },
+            }}
+            className='menu__animation-wrapper'
+          >
+            <NavMenu />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <Header toggleMenu={() => setMenuOpen(!menuOpen)} />
       {children}
-      <div className='scroll-progress' style={{ width: `${scrollProgress}vw` }}/>
+      <div
+        className='scroll-progress'
+        style={{ width: `${scrollProgress}vw` }}
+      />
       <Footer />
     </main>
   )
