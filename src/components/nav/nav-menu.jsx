5cec0a7bdd7a85b016121488dd40eaf7e48b0050
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-20 14:52:28 +0700

    Add NavMenu component.

---
+++ src/components/nav/nav-menu.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+import './nav-menu.scss'
+
+export const NavMenu = () => {
+  return <nav className='nav-menu'>Menu Options</nav>
+}
