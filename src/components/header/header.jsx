d800dfa0341c9056190c9e1fbcd1adff6b4a5d84
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-22 23:11:03 +0700

    Add logo and menu icon to header.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Logo } from '../svg/logo'
+import { MenuIcon } from '../svg/menu-icon'
 
 import './header.scss'
 
 export const Header = ({ toggleMenu }) => {
   return (
-    <nav className="header-nav">
-      <div className="logo">LOGO</div>
-      <div className="menu-button" onClick={toggleMenu}>MENU</div>
+    <nav className='header-nav'>
+      <div className='logo'><Logo /></div>
+      <div className='menu-button' onClick={toggleMenu}>
+        <span className='menu-text'>Menu</span>
+        <MenuIcon />
+      </div>
     </nav>
   )
 }
---
+++ src/components/svg/logo.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+export const Logo = () => {
+  return (
+    <svg
+      width='105'
+      height='114'
+      viewBox='0 0 105 114'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g clipPath='url(#clip0_462:3)'>
+        <path
+          d='M34.0309 32.1466V99.1757'
+          stroke='var(--white)'
+          strokeWidth='5.85502'
+          strokeLinecap='round'
+        />
+        <path
+          d='M48.4734 51.4479C48.4734 82.4184 62.2475 96.6108 101.238 99.3466'
+          stroke='var(--white)'
+          strokeWidth='5.85502'
+          strokeLinecap='round'
+        />
+        <path
+          d='M98.2342 19.0117C90.0996 -2.23509 44.4075 2.94792 32.1923 37.0561'
+          stroke='var(--white)'
+          strokeWidth='5.85502'
+        />
+        <path
+          d='M46.7113 51.3227C87.6038 52.6664 104.498 39.0369 98.1627 18.6889'
+          stroke='var(--white)'
+          strokeWidth='5.85502'
+        />
+        <path
+          d='M1.71008 11.2073C22.1351 11.2073 33.4646 9.77129 33.4646 37.6939'
+          stroke='var(--white)'
+          strokeWidth='5.23482'
+        />
+        <path
+          d='M32.9859 35.7785C11.4439 36.736 1.03838 30.4183 2.47454 9.03799'
+          stroke='var(--white)'
+          strokeWidth='5.23482'
+        />
+        <path
+          d='M75.9427 30.0856C59.1492 29.7344 48.9112 28.6797 48.431 51.638'
+          stroke='var(--white)'
+          strokeWidth='5.54915'
+        />
+        <path
+          d='M48.3116 50.3228C66.0071 51.4804 74.1721 47.1105 73.3588 29.5067'
+          stroke='var(--white)'
+          strokeWidth='5.54915'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_462:3'>
+          <rect width='105' height='113.197' fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
