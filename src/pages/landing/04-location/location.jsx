74d1d7b51bf1c0d4f08b420a3cbf74203cd5ba4f
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-22 23:16:18 +0700

    Location section gets placeholder map image and placeholder location addresses. Not finished.

--- src/pages/landing/04-location/location.jsx
+++ src/pages/landing/04-location/location.jsx
@@ -3,12 +3,66 @@ import { Container } from '../../../components/container/container'
 
 import './location.scss'
 
+const locations = [
+  {
+    name: 'Hải Châu',
+    address: `
+      Tòa nhà, 
+      Indochina Riverside Towers, Lầu 2, Đ. Trần Phú, 
+      Hải Châu, Đà Nẵng
+    `,
+    number: '+84.865.528.252',
+    coordinates: '',
+  },
+  {
+    name: 'An Thượng',
+    address: `
+      Tòa nhà, 
+      Indochina Riverside Towers, Lầu 2, Đ. Trần Phú, 
+      Hải Châu, Đà Nẵng
+    `,
+    number: '+84.865.528.252',
+    coordinates: '',
+  },
+  {
+    name: 'Sơn Trà',
+    address: `
+      Tòa nhà, 
+      Indochina Riverside Towers, Lầu 2, Đ. Trần Phú, 
+      Hải Châu, Đà Nẵng
+    `,
+    number: '+84.865.528.252',
+    coordinates: '',
+  },
+]
 
 export const Location = () => {
+
   return (
-    <section className="location">
-      <Container>
-        FIND US
+    <section className='location'>
+      <Container column>
+        <div className='location__grid'>
+          <div className='location__map'>
+            <img
+              src='https://res.cloudinary.com/jameswalker-work/image/upload/v1634903202/Roots/GPS-Map_uufti0.png'
+              alt='map to currently selected location'
+              className='map'
+            />
+          </div>
+          <div className='location__wrap'>
+            <ul className='locations'>
+              {locations.map(({ name, address, number }) => {
+                return (
+                  <li className='location-info' key={name}>
+                    <span>{name}</span>
+                    <span>{address}</span>
+                    <span>{number}</span>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
       </Container>
     </section>
   )
