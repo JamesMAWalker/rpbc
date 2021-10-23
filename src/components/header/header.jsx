98ea3c4182dc717d4f148f7285ebb5dba444f750
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-23 15:28:03 +0700

    Add animations to header.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,68 @@
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
 import { Logo } from '../svg/logo'
 import { MenuIcon } from '../svg/menu-icon'
+import { CloseBtn } from '../svg/close-button'
 
 import './header.scss'
 
-export const Header = ({ toggleMenu }) => {
+const switchAnimation = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.5,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: -100,
+    transition: {
+      delay: 0.5,
+    },
+  },
+}
+
+export const Header = ({ toggleMenu, menuOpen }) => {
   return (
     <nav className='header-nav'>
-      <div className='logo'><Logo /></div>
+      <div className='logo'>
+        <Logo />
+      </div>
       <div className='menu-button' onClick={toggleMenu}>
-        <span className='menu-text'>Menu</span>
-        <MenuIcon />
+        {menuOpen && (
+          <AnimatePresence>
+            <motion.div
+              className='icon-wrap'
+              variants={switchAnimation}
+              initial='hidden'
+              animate='visible'
+              exit='hidden'
+            >
+              <CloseBtn />
+              <span className='menu-text'>
+                Close
+              </span>
+            </motion.div>
+          </AnimatePresence>
+        )}{' '}
+        {!menuOpen && (
+          <AnimatePresence>
+            <motion.div
+              className='icon-wrap'
+              variants={switchAnimation}
+              initial='hidden'
+              animate='visible'
+              exit='hidden'
+            >
+              <MenuIcon />
+              <span className='menu-text'>
+                Menu
+              </span>
+            </motion.div>
+          </AnimatePresence>
+        )}
       </div>
     </nav>
   )
