84acd4b2d64960e9e3ca8e3eca5ce928708c7964
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-26 14:21:31 +0700

    BtnTextArrow gets direction prop; transform styles now reside in parent component.

--- src/components/buttons/btn__text-arrow.jsx
+++ src/components/buttons/btn__text-arrow.jsx
@@ -4,9 +4,11 @@ import { Arrow } from '../svg/arrow'
 
 import './btns.scss'
 
-export const BtnTextArrow = ({ children }) => {
+export const BtnTextArrow = ({ children, direction }) => {
+  const left = direction === 'left' ? 'rotate(180deg)' : 'rotate(0deg)'
+  
   return (
-    <button className='btn__text-arrow'>
+    <button className='btn__text-arrow' style={{ transform: left }}>
       { children }{' '}
       <span className='arrow-wrap'>
         <Arrow />
